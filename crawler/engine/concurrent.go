b5875584d9323972e0b95eb2d1f88202f01f20f4
oliverwangx <olivwang@umich.edu>
2020-06-05 01:45:56 -0400

    update concurrent version

---
+++ crawler/engine/concurrent.go
@@ -0,0 +1,37 @@
+package engine
+
+import "fmt"
+
+type ConcurrentEngine struct {
+	Scheduler   Scheduler
+	WorkerCount int
+}
+
+type Scheduler interface {
+	Submit(Request)
+}
+
+func (e ConcurrentEngine) Run(seeds ...Request) {
+	for _, r := range seeds {
+		e.Scheduler.Submit(r)
+	}
+
+	in := make(chan Request)
+	out := make(chan ParseResult)
+	for i := 0; i < e.WorkerCount; i++ {
+		createWorker(in, out)
+	}
+
+	for {
+		result := <-out
+		for _, item := range result.Items {
+			fmt.Printf("Got item: %v", item)
+		}
+
+		for _, request := range result.Requests {
+			e.Scheduler.Submit(request)
+
+		}
+	}
+
+}
---
+++ crawler/engine/simple.go
@@ -0,0 +1,40 @@
+package engine
+
+import (
+	"crawler/crawler/fetcher"
+	"log"
+)
+
+type SimpleEngine struct{}
+
+func (e SimpleEngine) Run(seeds ...Request) {
+	var requests []Request
+	for _, r := range seeds {
+		requests = append(requests, r)
+	}
+
+	for len(requests) > 0 {
+		r := requests[0]
+		requests = requests[1:]
+		ParseResult, error := e.worker(r)
+		if error != nil {
+			continue
+		}
+		requests = append(requests, ParseResult.Requests...)
+
+		for _, item := range ParseResult.Items {
+			log.Printf("Got item %s", item)
+		}
+	}
+}
+
+func (SimpleEngine) worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching %s", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error "+
+			"fetching url %s: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+	return r.ParserFunc(body), nil
+}
